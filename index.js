3cd45c3aca1a699df01bc4d08b3c2619244d7625
Emir Şener <emir@cbot.ai>
2023-01-27 16:16:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const dirname = '/Users/turkgen/.jitsi-meet-cfg/jibri/recordings/'
+
+app.get('/recordings/:recording_folder', (req, res) => {
+    const video_folder = dirname + req.params.recording_folder;
+    fs.readdir(video_folder, (err, files) => {
+        if (files) {
+            files.forEach(file => {
+                if (err) {
+                    res.status(500).send({ error: err })
+                }
+                else {
+                    if (file.endsWith('.mp4')) {
+                        res.download(video_folder + '/' + file);
+                    }
+                }
+            });
+        } else {
+            res.status(404).send({ message: 'File not found!' })
+        }
+    });
+});
+
+app.listen(5010);
